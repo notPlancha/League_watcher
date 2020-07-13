876197b949d325a5b36a3e614ff49dd8b7f5548c
notPlancha <andre.plancha@hotmail.com>
2020-07-13 18:18:56 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,184 @@
+from tkinter import *
+import time
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as cond
+from selenium.webdriver.support.ui import WebDriverWait
+
+def wait_for_element(driver, max_time_waiting, xpath):
+    try:
+        element = WebDriverWait(driver, max_time_waiting).until(cond.presence_of_element_located((By.XPATH, xpath)))
+        return element
+    except NoSuchElementException:
+        return None
+
+def check_exists_by_xpath(driver, xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    else:
+        return True
+
+def open_new_tab(driver):#Doesn't work headless
+    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+
+def wait_to_click(element, shutdown_time):
+    for i in range(shutdown_time):
+        try:
+            element.click()
+            return
+        except ElementNotInteractableException:
+            time.sleep(1)
+            continue
+    raise ElementNotInteractableException
+
+
+#start driver
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+webdriver_path = "C:/Users/Andre/Desktop/python/files/chromedriver.exe"
+driver = webdriver.Chrome(executable_path= webdriver_path, options= chrome_options)
+driver.maximize_window()
+
+#driver.get("https://leagueoflegends.fandom.com/wiki/List_of_champions")
+#champion_xpath = "//table[@class='wikitable sortable jquery-tablesorter']//tbody//*[contains(@style, 'text-align:left;')]"
+#champion_elements_list = driver.find_elements_by_xpath(champion_xpath)
+champions = ['aatrox', 'ahri', 'akali', 'alistar', 'amumu', 'anivia', 'annie', 'aphelios', 'ashe', 'aurelion sol', 'azir', 'bard', 'blitzcrank', 'brand', 'braum', 'caitlyn', 'camille', 'cassiopeia', "cho'gath", 'corki', 'darius', 'diana', 'dr. mundo', 'draven', 'ekko', 'elise', 'evelynn', 'ezreal', 'fiddlesticks', 'fiora', 'fizz', 'galio', 'gangplank', 'garen', 'gnar', 'gragas', 'graves', 'hecarim', 'heimerdinger', 'illaoi', 'irelia', 'ivern', 'janna', 'jarvan iv', 'jax', 'jayce', 'jhin', 'jinx', "kai'sa", 'kalista', 'karma', 'karthus', 'kassadin', 'katarina', 'kayle', 'kayn', 'kennen', "kha'zix", 'kindred', 'kled', "kog'maw", 'leblanc', 'lee sin', 'leona', 'lillia', 'lissandra', 'lucian', 'lulu', 'lux', 'malphite', 'malzahar', 'maokai', 'master yi', 'miss fortune', 'mordekaiser', 'morgana', 'nami', 'nasus', 'nautilus', 'neeko', 'nidalee', 'nocturne', 'nunu', 'olaf', 'orianna', 'ornn', 'pantheon', 'poppy', 'pyke', 'qiyana', 'quinn', 'rakan', 'rammus', "rek'sai", 'renekton', 'rengar', 'riven', 'rumble', 'ryze', 'sejuani', 'senna', 'sett', 'shaco', 'shen', 'shyvana', 'singed', 'sion', 'sivir', 'skarner', 'sona', 'soraka', 'swain', 'sylas', 'syndra', 'tahm kench', 'taliyah', 'talon', 'taric', 'teemo', 'thresh', 'tristana', 'trundle', 'tryndamere', 'twisted fate', 'twitch', 'udyr', 'urgot', 'varus', 'vayne', 'veigar', "vel'koz", 'vi', 'viktor', 'vladimir', 'volibear', 'warwick', 'wukong', 'xayah', 'xerath', 'xin zhao', 'yasuo', 'yorick', 'yuumi', 'zac', 'zed', 'ziggs', 'zilean', 'zoe', 'zyra']
+#for i in champion_elements_list:
+#    champions.append((i.get_attribute("data-sort-value")).lower())
+champion = False
+while champion == False:
+    championEntry = Tk()
+    champion = Entry(championEntry, borderwidth=2)
+    champion.pack()
+    button_clicked = False
+    def close(event):
+        global button_clicked
+        button_clicked = True
+    button = Button(championEntry, text="submit", command=close).pack()
+    championEntry.bind('<Return>', close)
+    while not button_clicked:
+        championEntry.update()
+    champion = champion.get()
+    if champion.lower() not in champions:
+        champion = True
+        Label(championEntry, text="Please enter valid champion").pack()
+    else:
+        championEntry.destroy()
+root = Tk()
+def removespaces(string):
+    returnstr = ""
+    for i in string:
+        if string != " ":
+            returnstr += i
+    return returnstr
+
+def write(text, column, row, letters=True):
+    if not letters:
+        textit = (''.join(filter(str.isdigit, text)))
+        if "," in text:
+            text = textit
+        elif "%" in text:
+            if len(textit) in [3, 4]:
+                text = (str(int(textit)/100)) + "%"
+            elif len(textit) == 2:
+                text = (str(int(textit)/10)) + "%"
+    text = removespaces(text)
+    label = Label(root, text= text)
+    label.grid(column = column, row = row)
+    root.update()
+
+
+colums = {
+    "champion" : 0,
+    "u.gg": 1,
+    "lolalytics":2,
+    "leagueofgraphs":3,
+}
+
+rows = {
+    "champion": 0,
+    "winrate": 1,
+    "pickrate": 2,
+    "banrate": 3,
+    "matches": 4,
+}
+
+
+for i in colums:
+    if i == "champion":
+        write(champion, 0, 0)
+    else:
+        write(i, colums[i], 0)
+
+for i in rows:
+    if i == "champion":
+        pass
+    else:
+        write(i, 0, rows[i])
+
+
+
+
+
+xpaths = {
+    "ugg": {
+        "cookies" : "//button[contains(text(),'I do not accept')] ",
+        "champion_input" : "//div[@class='search-container']//input[@id='super-search-bar']",
+        "search" : "//div[@class='search-container']//div[@class='autosuggest-button']",
+        "champion" : "//div[@class='champion-title-init']//span[@class='champion-name']",
+        "winrate": "//div[@class='win-rate meh-tier']//div[@class='value']",
+        "pickrate": "//div[@class='pick-rate']//div[@class='value']",
+        "banrate": "//div[@class='ban-rate']//div[@class='value']",
+        "matches": "//div[@class='matches']//div[@class='value']",
+        #"tier": "",
+        #"mainrole":"//div[contains(@class,'DESKTOP_LARGE ')]//div[contains(@class,'role-filter active')]",
+        #"tierlist":"//span[contains(text(),'LoL Tier List')]",
+    },
+    "lolalytics": {
+        "winrate": "/html[1]/body[1]/div[1]/div[3]/div[4]/div[4]/div[1]",
+        "pickrate":"/html[1]/body[1]/div[1]/div[3]/div[4]/div[4]/div[4]",
+        "banrate": "/html[1]/body[1]/div[1]/div[3]/div[4]/div[4]/div[5]",
+        "matches": "/html[1]/body[1]/div[1]/div[3]/div[4]/div[4]/div[6]",
+    },
+    "leagueofgraphs":{
+        "winrate": "/html[1]/body[1]/div[2]/div[3]/div[3]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]",
+        "pickrate":"/html[1]/body[1]/div[2]/div[3]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]",
+        "banrate": "/html[1]/body[1]/div[2]/div[3]/div[3]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]",
+        "matches": "//span[@id='matchesCountNumber']",
+    }
+}
+#u.gg
+
+
+
+driver.get("https://u.gg/lol/champions/%s/build"%(champion.lower()))
+element = wait_for_element(driver, 10, xpaths["ugg"]["cookies"])
+wait_to_click(element, 10)
+[write(driver.find_element_by_xpath(xpaths["ugg"][i]).text, colums["u.gg"], rows[i])
+ for i in
+ ["matches", "winrate", "pickrate", "banrate"]
+ ]
+
+driver.get("https://www.lolalytics.com/lol/%s/?tier=platinum_plus" %(champion.lower()))
+wait_for_element(driver, 10, xpaths["lolalytics"]["winrate"])
+[write(driver.find_element_by_xpath(xpaths["lolalytics"][i]).text, colums["lolalytics"], rows[i], letters=False)
+ for i in
+ ["winrate", "pickrate", "banrate", "matches"]
+ ]
+
+
+driver.get("https://www.leagueofgraphs.com/champions/stats/%s/sr-ranked" %(champion.lower()))
+wait_for_element(driver, 10, xpaths["leagueofgraphs"]["pickrate"])
+[write(driver.find_element_by_xpath(xpaths["leagueofgraphs"][i]).text, colums["leagueofgraphs"], rows[i])
+ for i in
+ ["matches", "pickrate", "banrate", "winrate"]]
+
+driver.quit()
+while True:
+    root.update()#buttons with sort?
